1fbcd239be729a4807080863778fd3b8599c0d27
125053029-ux <125053029@upq.edu.mx>
2026-09-17 17:14:52 -0700

    creacion de intro.js, vista.html, logica.js y introNode

---
+++ introJS/logica.js
@@ -0,0 +1,4 @@
+        
+        let nombre= prompt("Cual es tu nombre:");//prompt en js sirve solicitar algo y let declara variables
+        console.log("hola "+nombre+"!!");
+        document.write("<h1> Hola "+nombre+"!!</h1>");//incustrar h1 titulo
