ef5a5f3f7068a61078b2bf6c9c0ed7aff5082abb
125053029-ux <125053029@upq.edu.mx>
2026-09-18 17:03:11 -0700

    avance individual

---
+++ CAJA/caja.js
@@ -0,0 +1,20 @@
+const pedidos = [];
+let pacumulados = 0;
+
+function agregarPedido(nombre,precio){
+    pedidos.push(nombre); // push pone como ultimos valores al array y devuelv euna longitud
+    pacumulados = pacumulados + precio;
+    
+}
+agregarPedido('sopa' ,60);
+agregarPedido('pollo' ,100);
+agregarPedido('chilaquiles',40);
+agregarPedido('Hot cakes',30);
+agregarPedido('Hamburguesa',110);
+agregarPedido('Pure de papa',50);
+
+console.log("PEDIDOS:");
+console.log(pedidos);
+console.log("TOTAL ACUMULADO:");
+console.log(pacumulados);
+
